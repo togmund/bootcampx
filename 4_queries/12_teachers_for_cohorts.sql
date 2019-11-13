024ad634b1beb098f7432d92f8619adc983da4d5
Ted Ogmundson <togmund@gmail.com>
2019-11-13 01:55:50 +0000

    4_queries, answers to questions 11 through 13

---
+++ 4_queries/12_teachers_for_cohorts.sql
@@ -0,0 +1,18 @@
+
+-- Get the name of all teachers that performed an assistance request during a cohort.
+
+-- Select the instructor's name and the cohort's name.
+-- Don't repeat the instructor's name in the results list.
+-- Order by the instructor's name.
+-- This query needs to select data for a cohort with a specific name, use 'JUL02' for the cohort's name here.
+
+SELECT
+  t.name AS teach,
+  c.name AS class
+FROM assistance_requests a_r
+  JOIN students s ON a_r.student_id = s.id
+  JOIN cohorts c ON s.cohort_id = c.id
+  JOIN teachers t ON a_r.teacher_id = t.id
+WHERE c.name = 'JUL02'
+GROUP BY class, teach
+ORDER BY teach;
