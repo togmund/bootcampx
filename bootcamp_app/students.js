e142d3e5e10ef74df1c3eb17091bc0ee542bb49a
Ted Ogmundson <togmund@gmail.com>
2019-11-14 20:04:30 +0000

    protected against sql injections

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,13 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id, students.name, cohorts.name AS cohort_name
-FROM students
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name = '${process.argv[2]}'
-LIMIT 5;
-`)
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+  SELECT students.id, students.name, cohorts.name AS cohort_name
+  FROM students
+  JOIN cohorts ON cohorts.id = students.cohort_id
+  WHERE cohorts.name ILIKE $1
+  LIMIT $2;
+`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
