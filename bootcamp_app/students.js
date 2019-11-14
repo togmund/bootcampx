39310ac7f1f8e586edd443c9c50cad92c6e7497d
Ted Ogmundson <togmund@gmail.com>
2019-11-14 19:53:42 +0000

    added database queries

---
+++ bootcamp_app/students.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT students.id, students.name, cohorts.name AS cohort_name
+FROM students
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name = '${process.argv[2]}'
+LIMIT 5;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
